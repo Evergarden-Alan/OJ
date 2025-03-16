d4064127257e986c75e07730b31d9ebe3f928c5c
Alan <ve1.11@outlook.com>
2025-03-16 21:00:09 +0800

    实现了使用先序+中序重建二叉树的算法

--- DataStructure/tree/tree.cpp
+++ DataStructure/tree/tree.cpp
@@ -44,14 +44,14 @@ void Preorder_Traversal_recursion(Btree_node* root){
        Preorder_Traversal_recursion(root->right_child);
 }
 
-void Midorder_Traversal_recursion(Btree_node* root){
+void Inorder_Traversal_recursion(Btree_node* root){
        if (root==NULL)
        {
               return;
        }
-       Midorder_Traversal_recursion(root->left_child);
+       Inorder_Traversal_recursion(root->left_child);
        cout<<root->data;
-       Midorder_Traversal_recursion(root->right_child);
+       Inorder_Traversal_recursion(root->right_child);
 }
 
 void Lastorder_Traversal_recursion(Btree_node* root){
@@ -64,6 +64,63 @@ void Lastorder_Traversal_recursion(Btree_node* root){
        cout<<root->data;
 }
 
+
+Btree_node* create_node(int data) {
+       Btree_node* node = (Btree_node*)malloc(sizeof(Btree_node));
+       node->data = data;
+       node->left_child = NULL;
+       node->right_child = NULL;
+       return node;
+   }
+   
+   Btree_node* rebuild_with_preorder_inorder(
+       int preorder[], int inorder[],
+       int pre_start, int pre_end,
+       int in_start, int in_end
+   ) {
+       if (pre_start > pre_end || in_start > in_end) {
+           return NULL;
+       }
+   
+       // 1. 找到根节点在中序中的位置
+       int root_val = preorder[pre_start];
+       int root_idx = -1;
+       for (int i = in_start; i <= in_end; i++) {
+           if (inorder[i] == root_val) {
+               root_idx = i;
+               break;
+           }
+       }
+       if (root_idx == -1) {
+           return NULL; // 输入不合法
+       }
+   
+       // 2. 计算左子树和右子树的长度
+       int left_size = root_idx - in_start;
+       int right_size = in_end - root_idx;
+   
+       // 3. 创建根节点
+       Btree_node* root = create_node(root_val);
+   
+       // 4. 递归构建左子树和右子树
+       root->left_child = rebuild_with_preorder_inorder(
+           preorder, inorder,
+           pre_start + 1,          // 左子树前序起始
+           pre_start + left_size,  // 左子树前序结束
+           in_start,               // 左子树中序起始
+           root_idx - 1            // 左子树中序结束
+       );
+   
+       root->right_child = rebuild_with_preorder_inorder(
+           preorder, inorder,
+           pre_start + left_size + 1, // 右子树前序起始
+           pre_end,                   // 右子树前序结束
+           root_idx + 1,              // 右子树中序起始
+           in_end                     // 右子树中序结束
+       );
+   
+       return root;
+   }
 
 int main(){
        Btree_node* root = NULL;
@@ -79,9 +136,19 @@ int main(){
 
        Preorder_Traversal_recursion(root);
        cout<<endl;
-       Midorder_Traversal_recursion(root);
+       Inorder_Traversal_recursion(root);
        cout<<endl;
        Lastorder_Traversal_recursion(root);
        cout<<endl;
+       
+
+       int preorder[] = {5,4,3,2,1,6,7,8};
+       int inorder[] = {1,2,3,4,5,6,7,8};
+       Btree_node* root2 = rebuild_with_preorder_inorder(preorder,inorder,0,7,0,7);
+
+
+       Lastorder_Traversal_recursion(root2);
+       cout<<endl;
+
        return 0;
 }
